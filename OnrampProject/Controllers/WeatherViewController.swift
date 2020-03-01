1dce1ff710633cdc0d296e56f02254a1604644c9
hollywoodno <hollywoodno@gmail.com>
2020-02-29 20:01:01 -0800

    create outlets for weather view

--- OnrampProject/Controllers/WeatherViewController.swift
+++ OnrampProject/Controllers/WeatherViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    // MARK: - Properties
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    @IBOutlet weak var currentDateLabel: UILabel!
+    @IBOutlet weak var lastTimeUpdatedLabel: UILabel!
+    @IBOutlet weak var hourBlockWeatherSlider: UISlider!
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func onTapRefreshImage(_ sender: UITapGestureRecognizer) {
+        
+        print("Should fetch current weather")
+    }
+    
+    @IBAction func didChangeWeatherBlock(_ sender: UISlider) {
+        print("Weather block time changed: \(sender.value)")
+    }
+    
 
 }
 
