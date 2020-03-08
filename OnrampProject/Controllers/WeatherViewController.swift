27044864df50d82d5838a06d8286ff92c749720c
hollywoodno <hollywoodno@gmail.com>
2020-03-07 17:11:27 -0800

    move weather update call from weather view model nitializer

--- OnrampProject/Controllers/WeatherViewController.swift
+++ OnrampProject/Controllers/WeatherViewController.swift
@@ -8,85 +8,87 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
+  
+  // MARK: - Properties
+  
+  @IBOutlet weak var currentWeatherLabel: UILabel!
+  @IBOutlet weak var currentDateLabel: UILabel!
+  @IBOutlet weak var lastTimeUpdatedLabel: UILabel!
+  @IBOutlet weak var hourBlockWeatherSlider: UISlider!
+  @IBOutlet weak var modelImageView: UIImageView!
+  @IBOutlet weak var modelNameLabel: UILabel!
+  
+  private var weatherViewModel = WeatherViewModel()
+  private var hourlyWeatherViewModel: HourlyWeatherViewModel!
+  
+  // MARK: - Methods
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    // MARK: - Properties
+    NotificationCenter.default.addObserver(self, selector: #selector(onUpdateModelName(_:)), name: .didSetModelName, object: nil)
+    hourBlockWeatherSlider.maximumValue = Float(WeatherBlockTime.allCases.count) - 1
+    hourBlockWeatherSlider.minimumValue = 0
+    modelNameLabel.text = ModelImageViewModel.getModelName()!
+    weatherViewModel.delegate = self
     
-    @IBOutlet weak var currentWeatherLabel: UILabel!
-    @IBOutlet weak var currentDateLabel: UILabel!
-    @IBOutlet weak var lastTimeUpdatedLabel: UILabel!
-    @IBOutlet weak var hourBlockWeatherSlider: UISlider!
-    @IBOutlet weak var modelImageView: UIImageView!
-    @IBOutlet weak var modelNameLabel: UILabel!
+    updateWeather()
+    updateUI()
+  }
+  
+  @IBAction func onTapRefreshImage(_ sender: UITapGestureRecognizer) {
+    updateWeather()
+  }
+  
+  @IBAction func didChangeWeatherBlock(_ sender: UISlider) {
+    let index = Int(sender.value)
+    print("Slider new value: \(index)")
     
-    private var weatherViewModel: WeatherViewModel!
-    private var hourlyWeatherViewModel: HourlyWeatherViewModel!
-    
-    // MARK: - Methods
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(onUpdateModelName(_:)), name: .didSetModelName, object: nil)
-        hourBlockWeatherSlider.maximumValue = Float(WeatherBlockTime.allCases.count) - 1
-        hourBlockWeatherSlider.minimumValue = 0
-        modelNameLabel.text = ModelImageViewModel.getModelName()!
-        
-        updateWeather()
-        updateUI()
+    // TODO: Hourly forecast currently not available, will have
+    // to use seed data or hold off on this
+  }
+  
+  private func updateWeather() {
+    weatherViewModel.updateCurrentWeather(city: Constants.defaults.location.city, state: Constants.defaults.location.state)
+  }
+  
+  private func updateUI() {
+    DispatchQueue.main.async {
+      
+      guard self.weatherViewModel.weather != nil else {
+        return
+      }
+      
+      self.currentWeatherLabel.text = "\(self.weatherViewModel.currentTemp!)°"
+      self.currentDateLabel.text = self.weatherViewModel.currentDateAsString
+      self.lastTimeUpdatedLabel.text = self.weatherViewModel.lastUpdateTime
+      self.modelImageView.image = UIImage(named: self.weatherViewModel.modelImageDetails!.imageName)
+      self.modelNameLabel.text = ModelImageViewModel.getModelName()!
     }
-    
-    @IBAction func onTapRefreshImage(_ sender: UITapGestureRecognizer) {
-        updateWeather()
-    }
-    
-    @IBAction func didChangeWeatherBlock(_ sender: UISlider) {
-        let index = Int(sender.value)
-        print("Slider new value: \(index)")
-        
-        // TODO: Hourly forecast currently not available, will have
-        // to use seed data or hold off on this
-    }
-    
-    private func updateWeather() {
-        weatherViewModel = WeatherViewModel(city: "Fresno", state: "california")
-    }
-    
-    private func updateUI() {
+  }
+  
+  @objc func onUpdateModelName(_ notification: Notification) {
+    if let modelInfo = notification.userInfo as? [String: String] {
+      if let modelName = modelInfo["name"] {
         DispatchQueue.main.async {
-            
-            guard self.weatherViewModel.weather != nil else {
-                return
-            }
-            
-            self.currentWeatherLabel.text = "\(self.weatherViewModel.currentTemp!)°"
-            self.currentDateLabel.text = self.weatherViewModel.currentDateAsString
-            self.lastTimeUpdatedLabel.text = self.weatherViewModel.lastUpdateTime
-            self.modelImageView.image = UIImage(named: self.weatherViewModel.modelImageDetails!.imageName)
-            self.modelNameLabel.text = ModelImageViewModel.getModelName()!
+          self.modelNameLabel.text = modelName
         }
+      }
     }
-    
-    @objc func onUpdateModelName(_ notification: Notification) {
-        if let modelInfo = notification.userInfo as? [String: String] {
-            if let modelName = modelInfo["name"] {
-                DispatchQueue.main.async {
-                    self.modelNameLabel.text = modelName
-                }
-            }
-        }
-    }
+  }
 }
 
 extension WeatherViewController: WeatherViewModelDelegate {
-    func didUpdateModelImageDetails(_ weatherViewModel: WeatherViewModel, modelImageViewModel: ModelImageViewModel) {
-        //
-    }
-    
-    func didUpdateWeather(_ weatherViewModel: WeatherViewModel) {
-        updateUI()
-    }
-    
-    func didFailWithError(_ weatherViewModel: WeatherViewModel, _ error: Error) {
-        print("Error with viewModel: \(error)")
-    }
+  func didUpdateModelImageDetails(_ weatherViewModel: WeatherViewModel, modelImageViewModel: ModelImageViewModel) {
+    //
+  }
+  
+  func didUpdateWeather(_ weatherViewModel: WeatherViewModel) {
+    print("Weather view model gave view controller the delegate a weather update")
+    updateUI()
+  }
+  
+  func didFailWithError(_ weatherViewModel: WeatherViewModel, _ error: Error) {
+    print("Error with viewModel: \(error)")
+  }
 }
--- OnrampProject/ViewModels/WeatherViewModel.swift
+++ OnrampProject/ViewModels/WeatherViewModel.swift
@@ -8,153 +8,148 @@
 import Foundation
 
 protocol WeatherViewModelDelegate {
-    func didUpdateWeather(_ weatherViewModel: WeatherViewModel)
-    func didFailWithError(_ weatherViewModel: WeatherViewModel, _ error: Error)
-    func didUpdateModelImageDetails(_ weatherViewModel: WeatherViewModel, modelImageViewModel: ModelImageViewModel)
+  func didUpdateWeather(_ weatherViewModel: WeatherViewModel)
+  func didFailWithError(_ weatherViewModel: WeatherViewModel, _ error: Error)
+  func didUpdateModelImageDetails(_ weatherViewModel: WeatherViewModel, modelImageViewModel: ModelImageViewModel)
 }
 
 /**
  Manages a current weather object
  */
 class WeatherViewModel {
-    
-    // MARK: - Properties
-    
-    private let weatherNetworkService = WeatherNetworkService()
-    fileprivate let userDefaultsService = UserDefaultsService()
-    private(set) var weatherCity: String
-    private(set) var weatherState: String
-    private(set) var lastUpdateTime: String?
-    private var modelImageViewModel = ModelImageViewModel()
-    var delegate: WeatherViewModelDelegate?
-    
-    private(set) var weather: Weather? {
-        didSet {
-            guard weather != nil else {
-                return
-            }
-            
-            lastUpdateTime = currentTimeAsString
-            
-            userDefaultsService.storeInUserDefaults(item: weather)
-            delegate?.didUpdateWeather(self)
-        }
+  
+  // MARK: - Properties
+  
+  private let weatherNetworkService = WeatherNetworkService()
+  fileprivate let userDefaultsService = UserDefaultsService()
+  private(set) var weatherCity: String?
+  private(set) var weatherState: String?
+  private(set) var lastUpdateTime: String?
+  private var modelImageViewModel = ModelImageViewModel()
+  var delegate: WeatherViewModelDelegate?
+  
+  private(set) var weather: Weather? {
+    didSet {
+      guard weather != nil else {
+        return
+      }
+      
+      lastUpdateTime = currentTimeAsString
+      
+      userDefaultsService.storeInUserDefaults(item: weather)
+      delegate?.didUpdateWeather(self)
+    }
+  }
+  
+  var currentTemp: Int? {
+    guard weather != nil else {
+      return nil
     }
     
-    var currentTemp: Int? {
-        guard weather != nil else {
-            return nil
-        }
+    return weather?.temperature
+  }
+  
+  var getCurrentDate: String {
+    return currentDateAsString
+  }
+  
+  var modelImageDetails: BaeImage? {
+    
+    guard weather != nil else {
+      return nil
+    }
+    
+    let typeOfWeather = TypeOfWeather(for: weather!.temperature).category
+    
+    return modelImageViewModel.getImagefor(typeOfWeather: typeOfWeather)
+  }
+  
+  var currentDateAsString: String {
+    return (weather?.date.getDateAsString())!
+  }
+  
+  var currentTimeAsString: String {
+    return (weather?.date.getTimeAsString())!
+  }
+  
+  // MARK: - Methods
+  
+  init() {
+    modelImageViewModel.delegate = self
+    
+    if let lastKnownWeather = userDefaultsService.getFromUserDefaults(item: Constants.userDefaultKeys.lastKnownWeather) as? Weather {
+      self.weather = lastKnownWeather
+      lastUpdateTime = self.weather?.date.getTimeAsString()
+      delegate?.didUpdateWeather(self)
+    }
+    
+  }
+  
+  func updateCurrentWeather(city: String, state: String) {
+    weatherNetworkService.getCurrentWeather(city: city, state: state) { (data, error) in
+      
+      guard error == nil else {
+        self.delegate?.didFailWithError(self, error!)
+        return
+      }
+      
+      if let data = data {
+        let date = Date()
+        let currentHourBlock = HourlyWeatherViewModel.getCurrentWeatherBlockUsing(date: date)
         
-        return weather?.temperature
+        self.weather = Weather(date: date, temperature: Int(data.main.temp), hourlyTemps: [:], city: city, state: state, currentHourBlock: currentHourBlock)
+      }
     }
-    
-    var getCurrentDate: String {
-        return currentDateAsString
-    }
-    
-    var modelImageDetails: BaeImage? {
-        
-        guard weather != nil else {
-            return nil
-        }
-        
-        let typeOfWeather = TypeOfWeather(for: weather!.temperature).category
-        
-        return modelImageViewModel.getImagefor(typeOfWeather: typeOfWeather)
-    }
-    
-    var currentDateAsString: String {
-        return (weather?.date.getDateAsString())!
-    }
-    
-    var currentTimeAsString: String {
-        return (weather?.date.getTimeAsString())!
-    }
-    
-    // MARK: - Methods
-    
-    init(city: String, state: String) {
-        self.weatherCity = city
-        self.weatherState = state
-        modelImageViewModel.delegate = self
-        
-        if let lastKnownWeather = userDefaultsService.getFromUserDefaults(item: Constants.userDefaultKeys.lastKnownWeather) as? Weather {
-            self.weather = lastKnownWeather
-            lastUpdateTime = self.weather?.date.getTimeAsString()
-            delegate?.didUpdateWeather(self)
-        }
-        
-        updateCurrentWeather(city: city, state: state)
-    }
-    
-    func updateCurrentWeather(city: String, state: String) {
-        weatherNetworkService.getCurrentWeather(city: city, state: state) { (data, error) in
-            
-            guard error == nil else {
-                self.delegate?.didFailWithError(self, error!)
-                return
-            }
-            
-            if let data = data {
-                let date = Date()
-                let currentHourBlock = HourlyWeatherViewModel.getCurrentWeatherBlockUsing(date: date)
-                
-                self.weather = Weather(date: date, temperature: Int(data.main.temp), hourlyTemps: [:], city: city, state: state, currentHourBlock: currentHourBlock)
-                
-                self.userDefaultsService.storeInUserDefaults(item: self.weather)
-            }
-        }
-    }
-    
-    func getFormattedDate(_ date: Date) -> String {
-        return date.getDateAsString()
-    }
-    
-    func getHourlyTemps() -> [Int: Int] {
-        return weather?.hourlyTemps ?? [:]
-    }
-    
-    func getCurrentWeatherBlock() -> WeatherBlockTime {
-        return weather!.currentHourBlock
-    }
-    
+  }
+  
+  func getFormattedDate(_ date: Date) -> String {
+    return date.getDateAsString()
+  }
+  
+  func getHourlyTemps() -> [Int: Int] {
+    return weather?.hourlyTemps ?? [:]
+  }
+  
+  func getCurrentWeatherBlock() -> WeatherBlockTime {
+    return weather!.currentHourBlock
+  }
+  
 }
 
 // MARK: - Extensions
 
 extension Date {
-    func getDateAsString() -> String {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .short // or setLocalizeDateFromTemplate("")
-        
-        let dateAsString = formatter.string(from: self)
-        
-        return dateAsString
-    }
+  func getDateAsString() -> String {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short // or setLocalizeDateFromTemplate("")
     
-    func getTimeAsString() -> String {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        
-        let dateAsString = formatter.string(from: self)
-        
-        return dateAsString
-    }
+    let dateAsString = formatter.string(from: self)
     
-    func getHour() -> Int {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        formatter.dateFormat = "HH"
-        
-        let hour = formatter.string(from: self)
-        
-        return Int(hour)!
-    }
+    return dateAsString
+  }
+  
+  func getTimeAsString() -> String {
+    let formatter = DateFormatter()
+    formatter.timeStyle = .short
+    
+    let dateAsString = formatter.string(from: self)
+    
+    return dateAsString
+  }
+  
+  func getHour() -> Int {
+    let formatter = DateFormatter()
+    formatter.timeStyle = .short
+    formatter.dateFormat = "HH"
+    
+    let hour = formatter.string(from: self)
+    
+    return Int(hour)!
+  }
 }
 
 extension WeatherViewModel: ModelImageViewModelDelegate {
-    func modelName(_ modelImageViewModel: ModelImageViewModel, didChange: Bool) {
-        delegate?.didUpdateModelImageDetails(self, modelImageViewModel: modelImageViewModel)
-    }
+  func modelName(_ modelImageViewModel: ModelImageViewModel, didChange: Bool) {
+    delegate?.didUpdateModelImageDetails(self, modelImageViewModel: modelImageViewModel)
+  }
 }
