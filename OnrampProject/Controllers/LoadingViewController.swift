97fc244f539bbf8e4f330291e43ab24438c27b87
hollywoodno <hollywoodno@gmail.com>
2020-03-15 22:35:52 -0700

    update loading view to use weather view model

--- OnrampProject/Controllers/LoadingViewController.swift
+++ OnrampProject/Controllers/LoadingViewController.swift
@@ -15,12 +15,14 @@ class LoadingViewController: UIViewController {
     
     let networkService = WeatherNetworkService()
     let locationService = LocationService()
+    let weatherViewModel = WeatherViewModel(weather: nil)
     
     // MARK: - Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationService.startUpdatingLocation()
+        weatherViewModel.delegate = self
         loadingSpinner.layer.opacity = 0
         loadSpinner()
     }
@@ -39,40 +41,41 @@ class LoadingViewController: UIViewController {
     }
     
     func getCurrentLocation() {
-        if let currentLocation = locationService.currentLocation {
-            getCurrentWeather(lat: currentLocation.lat, long: currentLocation.long)
+        if locationService.currentLocation != nil {
+            //            getCurrentWeather(lat: currentLocation.lat, long: currentLocation.long)
+            weatherViewModel.getCurrentWeather()
         } else {
             print("Show there was an error getting current location")
         }
         locationService.stopUpdatingLocation()
     }
+}
+
+extension LoadingViewController: WeatherViewModelDelegate {
+    func didUpdateWeather(_ weatherViewModel: WeatherViewModel) {
+        self.loadingSpinner.stopAnimating()
+        
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        let weatherVC = storyboard.instantiateViewController(identifier: String(describing: WeatherViewController.self)) as! WeatherViewController
+        
+        weatherVC.weatherViewModel = WeatherViewModel(weather: weatherViewModel.weather)
+        weatherVC.weatherViewModel.delegate = weatherVC.self
+        
+        let settingsVC = storyboard.instantiateViewController(identifier: String(describing: SettingsViewController.self)) as! SettingsViewController
+        
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = [weatherVC, settingsVC]
+        tabBarController.modalPresentationStyle = .fullScreen
+        
+        self.present(tabBarController, animated: true, completion: nil)
+    }
     
-    func getCurrentWeather(lat: Double, long: Double) {
-        networkService.getCurrentWeather(lat: lat, long: long) { (weatherData, error) in
-            
-            self.loadingSpinner.stopAnimating()
-            
-            if error != nil {
-                print("loading weather returned an error, show this to user on loading screen")
-            }
-            
-            if let weatherData = weatherData {
-                
-                let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
-                let weatherVC = storyboard.instantiateViewController(identifier: String(describing: WeatherViewController.self)) as! WeatherViewController
-                
-                let weather = WeatherViewModel.makeWeather(from: weatherData)
-                weatherVC.weatherViewModel = WeatherViewModel(weather: weather)
-                weatherVC.weatherViewModel.delegate = weatherVC.self
-                
-                let settingsVC = storyboard.instantiateViewController(identifier: String(describing: SettingsViewController.self)) as! SettingsViewController
-                
-                let tabBarController = UITabBarController()
-                tabBarController.viewControllers = [weatherVC, settingsVC]
-                tabBarController.modalPresentationStyle = .fullScreen
-                
-                self.present(tabBarController, animated: true, completion: nil)
-            }
-        }
+    func didFailWithError(_ weatherViewModel: WeatherViewModel, _ error: Error) {
+        self.loadingSpinner.stopAnimating()
+        print("loading weather returned an error: \(error), show this to user on loading screen")
+    }
+    
+    func didUpdateModelImageDetails(_ weatherViewModel: WeatherViewModel, modelImageViewModel: ModelImageViewModel) {
+        //
     }
 }
